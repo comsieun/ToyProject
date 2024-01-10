95d77a5e5df4c1c1bf375f8af24e2b0c0754c30a
comsieun <ja24z8@naver.com>
2024-01-10 22:14:22 +0900

    채팅서버

---
+++ chat_server/client.py
@@ -0,0 +1,35 @@
+import socket
+from threading import Thread
+
+HOST = '172.30.1.32'
+PORT = 9124
+
+
+def rcvMsg(sock):
+    while True:
+        try:
+            data = sock.recv(65535)
+            if not data:
+                break
+            print(data.decode())
+        except:
+            pass
+
+
+def runChat():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        t = Thread(target=rcvMsg, args=(sock,))
+        t.daemon = True
+        t.start()
+
+        while True:
+            msg = input()
+            if msg == '/quit':
+                sock.send(msg.encode())
+                break
+
+            sock.send(msg.encode())
+
+
+runChat()
